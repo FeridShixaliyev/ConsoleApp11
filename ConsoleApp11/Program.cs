9d2c6c76fc5a75698decaf41fe479878bdf2b111
tu7b9ed4n <tu7b9ed4n@214-13>
2022-05-14 13:29:43 +0400

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Weapon weapon = new Weapon("Ak47",30,30,false);
+            //Console.WriteLine(weapon.NowCap);
+            //Console.WriteLine(weapon.Capacity);
+            //Console.WriteLine(weapon.Shoot());
+            //Console.WriteLine(weapon.Shoot());
+            //Console.WriteLine(weapon.Shoot());
+
+            //Console.WriteLine(weapon.Fire());
+            //Console.WriteLine(weapon.GetRemainBulletCount());
+            Console.WriteLine("Silahin adini daxil edin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Silahin capacity-ni daxil edin:");
+            int capacity ;
+            do
+            {
+                capacity = Convert.ToInt32(Console.ReadLine());
+            } while (capacity<0);
+            Console.WriteLine("Silahin cari gulle sayini daxil edin:");
+            int nowCap;
+            do
+            {
+                nowCap = Convert.ToInt32(Console.ReadLine());
+            } while (nowCap < 0||capacity<nowCap);
+            Weapon weapon = new Weapon(name, capacity, nowCap);
+            Console.WriteLine(weapon.ShowInfo());
+            Console.WriteLine("Atis modu(tekli yoxsa coxlu)");
+            Console.WriteLine("1. Tekli mod");
+            Console.WriteLine("2. Coxlu mod");
+            string mode = Console.ReadLine();
+            switch (mode)
+            {
+                case "1":
+                    weapon.Mode = false;
+                    weapon.Checkode();
+                    break;
+                case "2":
+                    weapon.Mode = true;
+                    weapon.Checkode();
+                    break;
+                default:
+                    Console.WriteLine("Duzgun secim edin");
+                    break;
+            }
+            
+
+            bool check = true;
+
+            do
+            {
+                Console.WriteLine("1. Info");
+                Console.WriteLine("2. Shoot");
+                Console.WriteLine("3. Fire");
+                Console.WriteLine("4. GetReamin");
+                Console.WriteLine("5. Reload");
+                Console.WriteLine("6. ChangeFireMode");
+                Console.WriteLine("7. Exit");
+                string choise = Console.ReadLine();
+                switch (choise)
+                {
+                    case "1":
+                        Console.WriteLine(weapon.ShowInfo());
+                        break;
+                    case "2":
+                        if (weapon.Mode == false)
+                        {
+                            Console.WriteLine(weapon.Shoot());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Silah coxli moddadir");
+                        }
+                        break;
+                    case "3":
+                        if (weapon.Mode == true)
+                        {
+                           Console.WriteLine(weapon.Fire());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Silah tekli moddadir");
+                        }
+                        
+                        break;
+                    case "4":
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        break;
+                    case "5":
+                        if (weapon.NowCap != weapon.Capacity)
+                        {
+                            weapon.Reload();
+                            Console.WriteLine(weapon.Capacity);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tam doludur");
+                        }
+                                                
+                        break;
+                    case "6":
+                        bool wep = false;
+                        do
+                        {
+                            Console.WriteLine("1. Single mode");
+                            Console.WriteLine("2. Selective fire");
+                            Console.WriteLine("0. Back");
+                            string ch = Console.ReadLine();
+                            switch (ch)
+                            {
+                                case "1":
+                                    if(weapon.Mode == false)
+                                    {
+                                        Console.WriteLine("Silah onsuz tekli moddadir");
+                                    }
+                                    else
+                                    {
+                                        weapon.Mode = false;
+                                    }
+                                    break;
+                                case "2":
+                                    if (weapon.Mode == true)
+                                    {
+                                        Console.WriteLine("Silah onsuz coxlu moddadir");
+                                    }
+                                    else
+                                    {
+                                        weapon.Mode = true;
+                                    }
+                                    break;
+                                default:
+                                    Console.WriteLine("Duzgun secim edin");
+                                    break;
+                            }
+                        } while (wep);
+                        break;
+                    case "7":
+                        check = false;
+                        break;
+                    default:
+                        Console.WriteLine("Duzgun secim edin");
+                        break;
+                }
+
+            } while (check);
+        }
+    }
+}
---
+++ ConsoleApp11/Weapon.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    class Weapon
+    {
+        public Weapon(string name,int capacity,int nowCap)
+        {
+            Id = _id++;
+            Name = name;
+            Capacity = capacity;
+            NowCap = nowCap;
+        }
+        private static int _id;
+        private int _capacity;
+        private int _nowCap;
+        public int Id { get; }
+        
+        public string Name { get; set; }
+        public int NowCap
+        {
+            get => _nowCap;
+            set
+            {
+                if (value >= 0 && value <= Capacity)
+                    _nowCap = value;
+            }
+        }
+
+
+        public int Capacity
+        {
+            get => _capacity;
+            set
+            {
+                if (value >= 0)
+                    _capacity = value;
+            }
+        }
+        public bool Mode = false;
+        public int Shoot()
+        {
+            return --NowCap;
+        }
+        public int Fire()
+        {
+            NowCap = 0;
+            Console.WriteLine($" Gulle bitti");
+            return 0;
+        }
+        public int GetRemainBulletCount()
+        {
+            return  Capacity-NowCap;
+        }
+        public void Reload()
+        {
+            NowCap = 30;
+        }
+        public string ShowInfo()
+        {
+            return $"\nWeapon info:\nName: {Name}\nCapacity: {Capacity}\nNowCap: {NowCap}\nMode:{Mode}\n";
+        }
+        public void Checkode()
+        {
+            if (Mode)
+            {
+                Shoot();
+            }
+            else
+            {
+                Mode = true;
+                Fire();
+            }
+        }
+    }
+}
